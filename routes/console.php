07526c12e2fa9c9c22bb5e2aaf0678b764a269d3
NadaMohamed2243 <106352195+NadaMohamed2243@users.noreply.github.com>
2025-03-27 17:55:42 +0200

    Modify Schedule part of the remider email

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// Define your scheduled command
+Schedule::command('login:reminder')
+    ->daily() // Runs once daily at midnight
+    ->at('09:00') // Or specify exact time
+    ->timezone('Africa/Cairo') // Set your timezone
+    ->emailOutputOnFailure('admin@example.com'); // Optional error notifications
