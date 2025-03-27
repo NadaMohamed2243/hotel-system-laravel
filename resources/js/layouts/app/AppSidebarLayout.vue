1f0fb515724bff9f334d3724823eff9127d06e35
dina <dinaelhadidy966@gmail.com>
2025-03-27 11:14:01 +0200

    Make AppSidebarLayout use ClientSidebar instead of AppSidebar

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import AppContent from '@/components/AppContent.vue';
 import AppShell from '@/components/AppShell.vue';
-import AppSidebar from '@/components/AppSidebar.vue';
+// import AppSidebar from '@/components/AppSidebar.vue';
+import AppSidebar from '@/components/ClientSidebar.vue';
 import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
 import type { BreadcrumbItemType } from '@/types';
 
